80fd9d5f41d92a7a0f25f16dbb3c540dc82e0e88
lebrancconvas <lebrancconvas@gmail.com>
2023-01-16 16:08:32 +0700

    ✨ feat: create word list page

---
+++ app/pages/lists.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>Word List</h1>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
