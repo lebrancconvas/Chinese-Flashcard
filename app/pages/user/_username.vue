0fafac8649a7bd9445f75f1f7d1e3952d3a2d06a
lebrancconvas <lebrancconvas@gmail.com>
2023-01-16 16:14:06 +0700

    ✨ feat: create user page

---
+++ app/pages/user/_username.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>User</h1>
+      </div>
+    </header>
+    <section id="user-section">
+      <div>
+        User: {{ $route.params.username }}
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  * {
+    text-align: center;
+  }
+</style>
---
+++ app/pages/user/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>Error Not Found Page</h1>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  * {
+    text-align: center;
+  }
+</style>
